e683588281c1da082843ae9f8d1bf0e5f3f8f962
strogyy <78235242+strogyy@users.noreply.github.com>
2022-05-21 02:28:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ let counter = 0;
 const back = document.querySelector(".back");
 const next = document.querySelector(".next");
 const sliderContainer = document.querySelector("#slider-container");
-var sliderContainerWidth =
-  document.querySelector("#slider-container").clientWidth;
+var slideWidth = document.querySelector(".slide").clientWidth;
 const slider = document.querySelector("#slider");
 const max = document.querySelectorAll(".slide").length - 1;
 const slideIcons = document.querySelectorAll(".slide-icon");
 
+//slide icon class func
 function activeClassRemove() {
   slideIcons.forEach((slideIcon) => {
     slideIcon.classList.remove("active");
@@ -16,7 +16,9 @@ function activeClassRemove() {
 function activeClassAdd() {
   slideIcons[counter].classList.add("active");
 }
+//slide icon class func
 
+//right arrow
 window.addEventListener("resize", sliderFunc);
 next.addEventListener("click", sliderFunc);
 
@@ -25,17 +27,18 @@ function sliderFunc() {
   activeClassRemove();
   if (counter < max) {
     counter++;
-    slider.style.left = "-" + sliderContainerWidth * counter + "px";
+    slider.style.left = "-" + slideWidth * counter + "px";
   } else {
     counter = 0;
-    slider.style.left = "-" + sliderContainerWidth * counter + "px";
+    slider.style.left = "-" + slideWidth * counter + "px";
   }
-  sliderContainerWidth =
-    document.querySelector("#slider-container").clientWidth;
+  slideWidth = document.querySelector(".slide").clientWidth;
 
   activeClassAdd();
 }
+//right arrow
 
+//left arrow
 window.addEventListener("resize", backEvent);
 back.addEventListener("click", backEvent);
 
@@ -44,22 +47,24 @@ function backEvent() {
 
   if (counter >= 1) {
     counter--;
-    slider.style.left = "-" + sliderContainerWidth * counter + "px";
+    slider.style.left = "-" + slideWidth * counter + "px";
   } else {
     counter = max;
-    slider.style.left = "-" + sliderContainerWidth * counter + "px";
+    slider.style.left = "-" + slideWidth * counter + "px";
   }
-  sliderContainerWidth =
-    document.querySelector("#slider-container").clientWidth;
+  slideWidth = document.querySelector(".slide").clientWidth;
 
   activeClassAdd();
 }
+//left arrow
 
 //while resizeing
+
 window.addEventListener("resize", () => {
   slider.style.transition = "none";
   clearInterval(timer);
 });
+//while resizeing
 
 //mousevnt
 
@@ -72,17 +77,20 @@ sliderContainer.addEventListener("mouseover", () => {
 sliderContainer.addEventListener("mouseout", () => {
   timer = setInterval(sliderFunc, 6000);
 });
+
 //mousevnt
 
+//slide icon toggle
 slideIcons[0].addEventListener("click", () => {
   activeClassRemove();
   counter = 0;
-  slider.style.left = "-" + sliderContainerWidth * counter + "px";
+  slider.style.left = "-" + slideWidth * counter + "px";
   activeClassAdd();
 });
 slideIcons[1].addEventListener("click", () => {
   activeClassRemove();
   counter = 1;
-  slider.style.left = "-" + sliderContainerWidth * counter + "px";
+  slider.style.left = "-" + slideWidth * counter + "px";
   activeClassAdd();
 });
+//slide icon toggle
